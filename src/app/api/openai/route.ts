cc72bd6939955885787c3197d21e3ad29a71a43a
Kuesung Park <gueit214@naver.com>
2023-05-28 19:41:41 +0900

    feat : openai - createFile

---
+++ src/app/api/openai/route.ts
@@ -0,0 +1,23 @@
+import { CSVLoader } from "langchain/document_loaders/fs/csv";
+import { NextResponse } from "next/server";
+const fs = require("fs");
+const { Configuration, OpenAIApi } = require("openai");
+const path = require("path");
+const configuration = new Configuration({
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const upLoadFile = async () => {
+  const response = await openai.createFile(
+    fs.createReadStream("public/data4.jsonl"),
+    "fine-tune"
+  );
+  console.log(response);
+  return response;
+};
+
+export async function GET() {
+  upLoadFile();
+  return NextResponse.json({ message: "Hello World" });
+}
