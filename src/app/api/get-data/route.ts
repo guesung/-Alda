525aadb552c0382d1e64dcf107e42c8e32887192
Kuesung Park <gueit214@naver.com>
2023-05-28 20:33:40 +0900

    feat : CSVLoader거쳐서 data반환 api

---
+++ src/app/api/get-data/route.ts
@@ -0,0 +1,13 @@
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { HNSWLib } from "langchain/vectorstores/hnswlib";
+import { NextResponse } from "next/server";
+import { CSVLoader } from "langchain/document_loaders/fs/csv";
+const OPENAI_API_KEY = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
+const embedding = new OpenAIEmbeddings({ openAIApiKey: OPENAI_API_KEY });
+
+export async function GET() {
+  const loader = new CSVLoader("public/data.csv");
+  const docs = await loader.load();
+  console.log(docs);
+  return NextResponse.json(docs);
+}
