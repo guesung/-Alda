288960fe116a89b1e2e685426623402f41a5efa1
Kuesung Park <gueit214@naver.com>
2023-05-28 21:24:58 +0900

    refactor : useEffect로 구현
    추후에 SSG로 구현 예정

--- src/app/openai/page.tsx
+++ src/app/openai/page.tsx
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 import { Configuration, OpenAIApi } from "openai";
-import { useRef, useState } from "react";
+import { use, useEffect, useState } from "react";
 
 const configuration = new Configuration({
   apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
@@ -17,18 +17,22 @@ interface csvDataType {
   pageContent: string;
 }
 
-const createChatCompletion = async (inputValue: string) => {
-  const { data } = await axios("/api/get-data");
-  const contentList = data.map((it: csvDataType) => it.pageContent);
+const createChatCompletion = async (database:csvDataType[], inputValue: string) => {
+  const contentList = database.map((it: csvDataType) => it.pageContent);
   const nameList = contentList.map(
     (it: string) => it.split("\n")[3].split(":")[1]
   );
-  const messageData = [];
-  messageData.push({
-    role: "system",
-    content: `Given the following extracted parts of a long document and a question, create a final answer.
+  const messageData = [
+    {
+      role: "system",
+      content: `Given the following extracted parts of a long document and a question, create a final answer.
   If you don't know the answer, just say that you don't know. Don't try to make up an answer.`,
-  });
+    },
+    {
+      role: "user",
+      content: inputValue + "약의 유의사항을 알려줘",
+    },
+  ];
 
   // 필요한 데이터 넣기
   const findDrugIndex = nameList.findIndex((name: string) =>
@@ -38,10 +42,6 @@ const createChatCompletion = async (inputValue: string) => {
 
   messageData.push({ role: "system", content: findDrug });
 
-  messageData.push({
-    role: "user",
-    content: inputValue + "약의 유의사항을 알려줘",
-  });
   console.log(messageData);
 
   const completion = await openai.createChatCompletion({
@@ -52,21 +52,28 @@ const createChatCompletion = async (inputValue: string) => {
 };
 
 export default function Page() {
-  const inputRef = useRef<HTMLInputElement>(null);
+  const [drugInput, setDrugInput] = useState<string>("");
+  const [database, setDatabase] = useState<csvDataType[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios("/api/get-data");
+      setDatabase(data);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          if (!inputRef.current) return;
-          createChatCompletion(inputRef.current.value);
+          createChatCompletion(database, drugInput);
         }}
       >
         <input
-          ref={inputRef}
           defaultValue={"세크로정"}
           width={500}
           type="text"
+          onChange={(e) => setDrugInput(e.target.value)}
         />
         <button>Click to run a chain</button>
       </form>
