c5ba67c9c66268e7f18ae4e789d1051ff93ed028
Kuesung Park <gueit214@naver.com>
2023-05-30 14:02:11 +0900

    feat : input에서 키보드 상하키 이동 감지 추가

--- src/app/openai/DrugInput.tsx
+++ src/app/openai/DrugInput.tsx
@@ -5,11 +5,16 @@ interface propsType {
   setDrugInput: Dispatch<SetStateAction<string>>;
 }
 function Header({ nameList, drugInput, setDrugInput }: propsType) {
-  const [autoCompleteWord, setAutoCompleteWord] = useState<string[]>([]);
+  const [autoCompleteWordList, setAutoCompleteWordList] = useState<string[]>(
+    []
+  );
+  const [keywordIndex, setKeywordIndex] = useState<number>(0);
+
+  console.log(keywordIndex);
 
   const updateData = () => {
     const b = nameList.filter((name) => name.includes(drugInput));
-    setAutoCompleteWord(b);
+    setAutoCompleteWordList(b);
   };
 
   useEffect(() => {
@@ -21,17 +26,35 @@ function Header({ nameList, drugInput, setDrugInput }: propsType) {
     };
   }, [drugInput]);
 
+  const controlKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    // console.log(e.keyCode)
+    switch (e.keyCode) {
+      case 38: // UP
+        if (keywordIndex === 0) break;
+        setKeywordIndex((keywordIndex) => keywordIndex - 1);
+        break;
+      case 40: // DOWN
+        if (keywordIndex >= autoCompleteWordList.length - 1) break;
+        setKeywordIndex((keywordIndex) => keywordIndex + 1);
+        break;
+      case 13: // ENTER
+        setKeywordIndex(0);
+        break;
+    }
+  };
   return (
     <div>
       <input
+        id="search"
         onChange={(e) => {
           setDrugInput(e.target.value);
         }}
         defaultValue={"세크로정"}
         type="text"
+        onKeyUp={controlKeyUp}
       />
       <div>
-        {autoCompleteWord.map((word: string, id: number) => (
+        {autoCompleteWordList.map((word: string, id: number) => (
           <div key={id}>{word}</div>
         ))}
       </div>
