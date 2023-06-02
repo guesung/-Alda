43923cc9d46e5c6592e87c8fc44022954c3de834
Kuesung Park <gueit214@naver.com>
2023-06-02 10:48:59 +0900

    refactor : px -> rem교체

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 
 export const metadata = {
   title: "Tech Tree",
-  description: "Welcome to Tech Tred!",
+  description: "Welcome to Tech Tree!",
   name: "viewport",
   content: "width=device-width, initial-scale=1",
 };
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className="flex h-[100dvh] w-screen justify-center bg-slate-50">
-        <div className="relative h-full w-full max-w-[450px] px-[1.375rem] bg-[#FCFDFF] text-[#242424] font-normal text-base leading-[1.375rem] font-sans">
+        <div className="relative h-full w-full max-w-[28.125rem] px-[1.375rem] bg-[#FCFDFF] text-[#242424] font-normal text-base leading-[1.375rem] font-sans">
           {children}
         </div>
       </body>
