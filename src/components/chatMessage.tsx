a146957ff75c177086c2cedf0deec14c0a250eee
Kuesung Park <gueit214@naver.com>
2023-06-02 09:26:54 +0900

    feat : chatMessage 말풍선 구현

--- src/components/chatMessage.tsx
+++ src/components/chatMessage.tsx
@@ -1,11 +1,18 @@
+import clsx from "clsx";
+
 interface PropsType {
   message: string;
   isMine?: boolean;
 }
 
-export default function ChatMessage({ message }: PropsType) {
+export default function ChatMessage({ message, isMine }: PropsType) {
   return (
-    <p className="px-4 py-2 bg-[#F3F6FF]  max-w-[15rem] rounded-r-[1.25rem] rounded-bl-[1.25rem] whitespace-pre-wrap inline-block">
+    <p
+      className={clsx(
+        "px-[1rem] py-[0.625rem] max-w-[15rem] rounded-r-[1.25rem] rounded-bl-[1.25rem] whitespace-pre-wrap inline-block",
+        isMine ? "bg-[#F3F6FF]" : "bg-[#C2E4EF]"
+      )}
+    >
       {message}
     </p>
   );
