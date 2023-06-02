d9a1ef9b71ca9678ccf75db4bb2b3c57d73db16c
Kuesung Park <gueit214@naver.com>
2023-06-02 16:18:25 +0900

    refactor : 불필요 코드 제거

--- src/app/api/get-data/route.ts
+++ src/app/api/get-data/route.ts
@@ -1,9 +1,5 @@
-import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { HNSWLib } from "langchain/vectorstores/hnswlib";
-import { NextResponse } from "next/server";
 import { CSVLoader } from "langchain/document_loaders/fs/csv";
-const OPENAI_API_KEY = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
-const embedding = new OpenAIEmbeddings({ openAIApiKey: OPENAI_API_KEY });
+import { NextResponse } from "next/server";
 
 export async function GET() {
   const loader = new CSVLoader("public/data.csv");
