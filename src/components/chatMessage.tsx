91e3e642289aceaa65706b729a96d5f72fdbdd1e
Kuesung Park <gueit214@naver.com>
2023-06-02 12:00:11 +0900

    feat : 스피커(TTS) 추가

--- src/components/chatMessage.tsx
+++ src/components/chatMessage.tsx
@@ -1,4 +1,6 @@
+import { makeTTS } from "@utils/makeTTS";
 import clsx from "clsx";
+import Image from "next/image";
 
 interface PropsType {
   message: string;
@@ -6,14 +8,34 @@ interface PropsType {
 }
 
 export default function ChatMessage({ message, isMine }: PropsType) {
+  const handleSpeaker = () => {
+    makeTTS(message);
+  };
   return (
-    <p
-      className={clsx(
-        "px-[1rem] py-[0.625rem] max-w-[15rem] rounded-r-[1.25rem] rounded-bl-[1.25rem] whitespace-pre-wrap inline-block",
-        isMine ? "bg-[#F3F6FF]" : "bg-[#C2E4EF]"
-      )}
+    <div
+      className={clsx("flex", isMine ? "flex-row-reverse" : "flex-row", "mb-5")}
     >
-      {message}
-    </p>
+      <p
+        className={clsx(
+          "px-[1rem] py-[0.625rem] max-w-[15rem] rounded-b-[1.25rem] whitespace-pre-wrap inline-block",
+          isMine
+            ? "bg-[#C2E4EF] rounded-tl-[1.25rem]"
+            : "bg-[#F3F6FF] rounded-tr-[1.25rem]"
+        )}
+      >
+        {message}
+      </p>
+      {!isMine && (
+        <div className="flex flex-col-reverse ml-2">
+          <Image
+            alt="speaker"
+            src="/icons/speaker.svg"
+            width={35}
+            height={35}
+            onClick={handleSpeaker}
+          />
+        </div>
+      )}
+    </div>
   );
 }
---
+++ src/types/chat.ts
@@ -0,0 +1,5 @@
+export interface chatMessage {
+  id: number;
+  message: string;
+  isMine: boolean;
+}
