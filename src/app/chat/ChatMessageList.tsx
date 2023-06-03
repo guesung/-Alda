9c806fb537034c7a27c068b70a482fd7e88f39c8
Kuesung Park <gueit214@naver.com>
2023-06-03 10:43:29 +0900

    feat : 채팅 목록에 질문지 선택 추가

--- src/app/chat/ChatMessageList.tsx
+++ src/app/chat/ChatMessageList.tsx
@@ -3,29 +3,58 @@
 import ChatMessage from "@components/chatMessage";
 import { useEffect } from "react";
 import { useRecoilState } from "recoil";
-import { chatMessageListState } from "store";
-import { chatMessageType } from "types/chat";
+import { chatMessageListState, userInfoState } from "store";
+import { chatMessageType, userInfoType } from "types/chat";
+import SelectMessage from "./SelectMessage";
 
 interface PropsType {
   chatMessageListProps: chatMessageType[];
+  userInfoProps: userInfoType;
+  nameList: string[];
+  contentList: string[];
 }
 
-export default function ChatMessageList({ chatMessageListProps }: PropsType) {
+export default function ChatMessageList({
+  chatMessageListProps,
+  userInfoProps,
+  nameList,
+  contentList,
+}: PropsType) {
   const [chatMessageList, setChatMessageList] =
     useRecoilState(chatMessageListState);
+  const [userInfo, setUserInfo] = useRecoilState(userInfoState);
+
   useEffect(() => {
     setChatMessageList(chatMessageListProps);
-  }, [chatMessageListProps, setChatMessageList]);
+    setUserInfo(userInfoProps);
+  }, [chatMessageListProps, setChatMessageList, setUserInfo, userInfoProps]);
 
   return (
     <article>
-      {chatMessageList.map((chatMessage) => (
-        <ChatMessage
-          message={chatMessage.message}
-          isMine={chatMessage.isMine}
-          key={chatMessage.id}
-        />
-      ))}
+      {chatMessageList.map((chatMessage: chatMessageType, index: number) => {
+        if (
+          chatMessage.isMine === false &&
+          index === chatMessageList.length - 1
+          // && index > 0
+        )
+          return (
+            <div key={chatMessage.id}>
+              <ChatMessage
+                message={chatMessage.message}
+                isMine={chatMessage.isMine}
+                key={chatMessage.id}
+              />
+              <SelectMessage nameList={nameList} contentList={contentList} />
+            </div>
+          );
+        return (
+          <ChatMessage
+            message={chatMessage.message}
+            isMine={chatMessage.isMine}
+            key={chatMessage.id}
+          />
+        );
+      })}
     </article>
   );
 }
