988012f1c73093b6e0cafa16796ae847503ba8d9
Kuesung Park <gueit214@naver.com>
2023-06-04 16:31:48 +0900

    refactor : 경로 수정

--- src/app/api/get-data/route.ts
+++ src/app/api/get-data/route.ts
@@ -1,8 +1,11 @@
 import { CSVLoader } from "langchain/document_loaders/fs/csv";
 import { NextResponse } from "next/server";
 
+const fs = require("fs");
+const path = require("path");
+
 export async function GET() {
-  const loader = new CSVLoader("public/data.csv");
+  const loader = new CSVLoader("public/data/data.csv");
   const docs = await loader.load();
   return NextResponse.json(docs);
 }
