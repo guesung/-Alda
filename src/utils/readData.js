7bb7249802adab95e377e8262f34833174985a47
Kuesung Park <gueit214@naver.com>
2023-06-04 16:31:09 +0900

    feat : JSON데이터 읽어오는 함수

---
+++ src/utils/readData.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const path = require("path");
+const savePath = path.join(__dirname, "../../public/data");
+const saveFileName = path.join(savePath, "data.js");
+const readData = fs.readFileSync(saveFileName);
+const data = JSON.parse(readData.toString())
+console.log(data)
+
