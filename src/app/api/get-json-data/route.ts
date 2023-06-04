27416b26efb0c90175c0ddf1651b2b7f6dc621ba
Kuesung Park <gueit214@naver.com>
2023-06-04 18:37:39 +0900

    feat : Next.js api에 params추가

--- src/app/api/get-json-data/route.ts
+++ src/app/api/get-json-data/route.ts
@@ -1,10 +1,12 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const fs = require("fs");
 const path = require("path");
 
-export async function GET() {
-  const saveFileName = path.join("public/data/data.js");
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const page = searchParams.get("page");
+  const saveFileName = path.join(`public/data/data${page}.js`);
   const readData = fs.readFileSync(saveFileName);
   return NextResponse.json(JSON.parse(readData.toString()));
 }
