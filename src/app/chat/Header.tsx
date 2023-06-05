511cb976ab75ac285f35e02c025270299343e851
Kuesung Park <gueit214@naver.com>
2023-06-05 18:13:42 +0900

    refactor : return문 수정

--- src/app/chat/Header.tsx
+++ src/app/chat/Header.tsx
@@ -13,22 +13,18 @@ export default function Header() {
     }
   }, []);
 
-  return (
-    <div>
-      {deviceType !== "unknown" ? (
-        <div></div>
-      ) : (
-        <header className="h-[3.5rem] flex justify-center items-center relative">
-          <p className="text-[1.25rem]">알다와 대화중</p>
-          <Image
-            alt="home"
-            src="/icons/close.svg"
-            width={15}
-            height={15}
-            className="absolute right-0"
-          />
-        </header>
-      )}
-    </div>
+  return deviceType !== "unknown" ? (
+    <div></div>
+  ) : (
+    <header className="h-[3.5rem] flex justify-center items-center relative">
+      <p className="text-[1.25rem]">알다와 대화중</p>
+      <Image
+        alt="home"
+        src="/icons/close.svg"
+        width={15}
+        height={15}
+        className="absolute right-0"
+      />
+    </header>
   );
 }
