b73eabb3773a8e46e0ba2c6c0a65f87fbb970ce7
Kuesung Park <gueit214@naver.com>
2023-06-05 15:57:58 +0900

    feat : 100개씩 45번 -> 10개씩 450번으로 수정

--- src/utils/getOpenData.ts
+++ src/utils/getOpenData.ts
@@ -3,7 +3,7 @@ const path = require("path");
 
 export const getOpenData = async (page: number) => {
   const response = await fetch(
-    `${"http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList"}?ServiceKey=${"dLIzfarVErjrIRr1G76y2ayIhVjZy4xWEcgncqSmGas%2BxJXxSVR4oEGCegUszVf6iBHXUakRT4Rho6MEvlSv6g%3D%3D"}&type=json&pageNo=${page}&numOfRows=${100}`
+    `${"http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList"}?ServiceKey=${"dLIzfarVErjrIRr1G76y2ayIhVjZy4xWEcgncqSmGas%2BxJXxSVR4oEGCegUszVf6iBHXUakRT4Rho6MEvlSv6g%3D%3D"}&type=json&pageNo=${page}&numOfRows=${10}`
   );
 
   if (response.ok) {
